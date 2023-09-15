becc60e00c2000b089b87647c1b5a43b74314802
Ben Hoyle <benjhoyle@gmail.com>
2023-09-15 14:29:19 +0100

    Working section parsing function.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,9 @@
 """Test for utility functions in utils.py."""
 
 import pytest
-from utils import fetch_xml
 from bs4 import BeautifulSoup
+from utils import fetch_xml, parse_recursive
+
 
 
 def test_fetch_xml_success(mocker):
@@ -22,3 +23,42 @@ def test_fetch_xml_exception(mocker):
     mocker.patch('requests.get', side_effect=Exception("An error occurred."))
     result = fetch_xml('http://test.com')
     assert result is None
+
+
+def test_parse_recursive():
+    with open('test_section.xml', 'r') as f:
+        soup = BeautifulSoup(f, 'lxml-xml')
+
+    # primary_element = soup.find('Primary')
+    primary_element = soup.find('P1')
+    parsed_data = parse_recursive(primary_element)
+
+    assert parsed_data['label'] == "1"
+    assert len(parsed_data['text']) == 5
+
+    # Assertions for the first subpoint
+    assert isinstance(parsed_data['text'][0]['text'][0], str)
+    assert isinstance(parsed_data['text'][0]['text'][5], str)
+    assert isinstance(parsed_data['text'][0]['text'][1], dict)
+    assert isinstance(parsed_data['text'][0]['text'][2], dict)
+    assert isinstance(parsed_data['text'][0]['text'][3], dict)
+    assert isinstance(parsed_data['text'][0]['text'][4], dict)
+    assert parsed_data['text'][0]['text'][0] == ('A patent may be granted only for an invention in respect of which '
+                                                 'the following conditions are satisfied, that is to say—')
+    assert parsed_data['text'][0]['text'][1]['label'] == "a"
+    assert parsed_data['text'][0]['text'][1]['text'] == ["the invention is new;"]
+    assert parsed_data['text'][0]['text'][5] == ('and references in this Act to a patentable invention '
+                                                 'shall be construed accordingly.')
+
+    for i, item in enumerate(parsed_data['text'], start=1):
+        assert item['label'] == str(i)
+
+    with open('test_section_2.xml', 'r') as f:
+        soup = BeautifulSoup(f, 'lxml-xml')
+
+    primary_element = soup.find('P1')
+    parsed_data = parse_recursive(primary_element)
+    assert parsed_data['label'] == "129"
+    assert len(parsed_data['text']) == 1
+    assert parsed_data['text'][0] == ('—This Act does not affect Her Majesty in her private capacity but, '
+                                      'subject to that, it binds the Crown.')
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 """Common utility functions."""
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
-from typing import Union
+from typing import Union, Dict, List
 
 
 def fetch_xml(base_url: str) -> Union[BeautifulSoup, None]:
@@ -33,3 +33,118 @@ def fetch_xml(base_url: str) -> Union[BeautifulSoup, None]:
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+
+
+def parse_element(element, depth: int = 0, parent_label: str = "") -> dict:
+    """
+    Recursively parses an XML element to extract legislative text and metadata.
+
+    Parameters:
+    - element: The XML element to parse (BeautifulSoup object)
+    - depth: The current depth level in the XML tree (default is 0)
+    - parent_label: The label for the parent element, used for nested elements (default is "")
+
+    Returns:
+    - dict: A dictionary containing the text and metadata for the legislative section
+    """
+    parsed_data = {
+        'label': "",
+        'full_label': "",
+        'number': 0,
+        'text': "",
+        'subpoints': []
+    }
+
+    # Extract the label and number based on the depth
+    if element.find("Pnumber"):
+        label_text = element.find('Pnumber').text
+        parsed_data['full_label'] = f"{parent_label}{label_text})"
+        parsed_data['label'] = f"{label_text})"
+        parsed_data['number'] = depth
+
+    # Extract the text
+    if element.find("Text"):
+        parsed_data['text'] = ' '.join(element.find("Text").text.strip().split())
+
+    # Recursively parse sub-elements
+    for sub_element in element.find_all(["P2", "P3"], recursive=False):
+        sub_parsed_data = parse_element(sub_element, depth=depth+1, parent_label=parsed_data['label'])
+        parsed_data['subpoints'].append(sub_parsed_data)
+
+    return parsed_data
+
+
+def parse_recursive_old(element: Union[BeautifulSoup, None], depth: int = 1) -> Union[Dict, List]:
+    if element is None:
+        return {}
+
+    parsed_data = {'subpoints': []}
+
+    # Start looking for nested elements
+    tag_name = f"P{depth}"
+    para_tag_name = f"P{depth}para"
+    nested_elements = element.find_all(tag_name)
+    if nested_elements:
+        for nested_element in nested_elements:
+            number_element = nested_element.find(f"Pnumber")
+            number = number_element.text.strip() if number_element else None
+
+            # Look for <Pxpara> element
+            para_element = nested_element.find(para_tag_name)
+            text_element = nested_element.find("Text")
+            text = ' '.join(element.find("Text").text.strip().split()) if text_element else None
+
+            nested_data = parse_recursive(nested_element.find(f"P{depth}para"), depth=depth + 1)
+
+            parsed_data['subpoints'].append({
+                'label': number,
+                'text': text,
+                **nested_data
+            })
+
+    return parsed_data
+
+
+def parse_recursive(element: Union[Tag, None], depth: int = 1) -> Union[Dict, List]:
+    """
+    Recursively parses an XML element to extract legislative text and metadata.
+
+    Parameters:
+        - element: The XML element to parse (BeautifulSoup object)
+        - depth: The current depth level in the XML tree (default is 0)
+
+    Returns:
+        - Union[Dict, List]: A dictionary containing the text and metadata for the legislative section
+        or a list of text/dictionary elements for nested sections.
+        """
+    if element is None:
+        return {}
+
+    parsed_data = {'text': []}
+
+    # Start looking for nested elements
+    para_tag_name = f"P{depth}para"
+
+    useful_child_tags = ["Pnumber", para_tag_name]
+    useful_children = element.find_all(useful_child_tags, recursive=False)
+    if useful_children:
+        for child in useful_children:
+            # Check if the tag is a paragraph number
+            if child.name == "Pnumber":
+                parsed_data['label'] = child.text.strip()
+
+            # Check if the tag is a paragraph content element
+            elif child.name == para_tag_name:
+                # Check for either text or deeper nested elements
+                next_level = f"P{depth + 1}"
+                para_children = child.find_all([next_level, "Text"], recursive=False)
+                if para_children:
+                    for para_child in para_children:
+                        if para_child.name == next_level:
+                            nested_data = parse_recursive(para_child, depth=depth + 1)
+                            parsed_data['text'].append(nested_data)
+                        elif para_child.name == "Text":
+                            text = ' '.join(para_child.text.strip().split())
+                            parsed_data['text'].append(text)
+
+    return parsed_data
